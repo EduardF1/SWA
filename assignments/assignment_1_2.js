0b731d4f8a851c91033866c360bf2f9b89307e0e
EduardF1 <fischer_eduard@yahoo.com>
2021-09-15 23:54:25 +0200

    done for now, remaining vigilant....

--- assignments/assignment_1_2.js
+++ assignments/assignment_1_2.js
@@ -1,3 +1,18 @@
+// global constants
+const NEW_LINE = '\n';
+const MM = 'MM';
+const MM_TYPE = 'Millimeters';
+const IN = 'IN';
+const IN_TYPE = 'Inches';
+const MPH = 'MPH';
+const MPH_TYPE = 'Miles per Hour';
+const MPS = 'MS';
+const MPS_TYPE = 'Meters per Second';
+const FAHRENHEIT_TYPE = 'Fahrenheit';
+const FAHRENHEIT_UNIT = '*F';
+const CELSIUS_TYPE = 'Celsius';
+const CELSIUS_UNIT = '*C';
+
 class Event {
     constructor(time, place) {
         this.time = time;
@@ -214,10 +229,9 @@ class CloudCoveragePrediction extends WeatherPrediction {
 }
 
 
-
 // Tests
 const weatherData1 = new WeatherData('September 13, 2021 14:11:00', 'Monte Casino', 30, 'Celsius', '*C');
-const temperature1 = new Temperature('May 1, 1997 24:00:00', 'Bucharest C',  'Celsius', '*C', 40);
+const temperature1 = new Temperature('May 1, 1997 24:00:00', 'Bucharest C', 'Celsius', '*C', 40);
 const precipitation1 = new Precipitation('September 13, 1854 04:11:00', 'Harkov', 22, 'Fahrenheit', '*F', 'rain');
 const wind1 = new Wind('December 11, 2021 18:11:00', 'Istanbul', 30, 'Celsius', '*C');
 const cloudCoverage = new CloudCoverage('January 2, 2021 01:03:04', 'Barcelona', 44, 'Fahrenheit', '*F');
@@ -270,12 +284,222 @@ console.log(cloudCoveragePrediction1);
 console.log(cloudCoveragePrediction1.getCloudCoverage());
 
 
-class WeatherHistory{
-    constructor(data, place, type, period) {
-        this.whData = data;
-        this.whPlace = new Event(place);
-        this.whType = type;
-        this.whPeriod = new DateInterval(period.dateFrom, period.dateTo);
+class WeatherHistory {
+    constructor(data) {
+        this.data = data;
     }
 
+    forPlace = (place) => {
+        let weatherHistoryOfPlace = [];
+        this.data.forEach((element) => {
+            if (element.getPlace() === place) {
+                weatherHistoryOfPlace.push(element);
+            }
+        });
+        return new WeatherHistory(weatherHistoryOfPlace);
+    }
+    forType = (type) => {
+        let weatherHistoryOfType = [];
+        this.data.forEach((element) => {
+            if (element.getType() === type) {
+                weatherHistoryOfType.push(element);
+            }
+        });
+        return new WeatherHistory(weatherHistoryOfType);
+    };
+    forPeriod = (period) => {
+        let weatherHistoryForPeriod = [];
+        this.data.forEach((element) => {
+                if (period.getDateFrom().getTime() <= element.getDate().getTime() &&
+                    element.getDate().getTime() <= period.getDateTo().getTime())
+                    weatherHistoryForPeriod.push(element);
+            }
+        );
+        return new WeatherHistory(weatherHistoryForPeriod);
+    };
+    including = (weatherData) => {
+        let weatherDataForComparison = [];
+        this.data.forEach(element => {
+            weatherData.forEach((comparisonElement => {
+                if (Object.keys(element).length === Object.keys(comparisonElement).length
+                    && Object.keys(element).every(p => element[p] === comparisonElement[p])) {
+                    weatherDataForComparison.push(element);
+                }
+            }));
+        });
+        return new WeatherHistory(weatherDataForComparison);
+    };
+    convertToUsUnits = () => {
+        this.data.forEach((element => {
+            switch (element.getType()) {
+                case CELSIUS_TYPE:
+                    element.setUnit(FAHRENHEIT_UNIT);
+                    element.setType(FAHRENHEIT_TYPE);
+                    element.setValue((element.getValue() * 9 / 5) + 32);
+                    break;
+                case MM_TYPE:
+                    element.setUnit(IN);
+                    element.setType(IN_TYPE);
+                    element.setValue(element.getValue() * 25.4);
+                    break;
+                case MPS_TYPE:
+                    element.setUnit(MPH)
+                    element.setType(MPH_TYPE);
+                    element.setValue(element.getValue() * 2.237)
+                    break;
+                default:
+                    break;
+            }
+        }));
+    };
+    convertToInternationalUnits = () => {
+        this.data.forEach((element => {
+            switch (element.getType()) {
+                case FAHRENHEIT_TYPE:
+                    element.setUnit(CELSIUS_UNIT);
+                    element.setType(CELSIUS_TYPE);
+                    element.setValue((element.getValue() * 9 / 5) + 32);
+                    break;
+                case IN_TYPE:
+                    element.setUnit(MM);
+                    element.setType(MM_TYPE);
+                    element.setValue(element.getValue() * 25.4);
+                    break;
+                case MPH_TYPE:
+                    element.setUnit(MPS)
+                    element.setType(MPS_TYPE);
+                    element.setValue(element.getValue() * 2.237)
+                    break;
+                default:
+                    break;
+            }
+        }));
+    };
+    lowestValue = () => this.data.reduce((previous, current) => previous.getValue() < current.getValue() ? previous : current);
+    getData = () => this.data.weatherDataSet;
+    setData = (weatherData) => this.data.weatherDataSet.concat(weatherData);
 }
+
+const weatherSet = [
+    new WeatherData(new Date('1000-01-01'), 'Kiev', 100, CELSIUS_TYPE, CELSIUS_UNIT),
+    new WeatherData(new Date('2011-01-01'), 'Dniepr', 200, CELSIUS_TYPE, CELSIUS_UNIT),
+]
+const weatherHistory1 = new WeatherHistory(weatherSet);
+console.log(
+    weatherHistory1.lowestValue().getValue()
+);
+
+
+class WeatherForecast {
+    constructor(data) {
+        this.weatherForecastsSet = data;
+    }
+
+    forPlace = (place) => {
+        let weatherForecastOfPlace = [];
+        this.weatherForecastsSet.forEach((element) => {
+            if (element.getPlace() === place) {
+                weatherForecastOfPlace.push(element);
+            }
+        });
+        return new WeatherForecast(weatherForecastOfPlace);
+    }
+    forType = (type) => {
+        let weatherHistoryOfType = [];
+        this.weatherForecastsSet.forEach((element) => {
+            if (element.getType() === type) {
+                weatherHistoryOfType.push(element);
+            }
+        });
+        return new WeatherForecast(weatherHistoryOfType);
+    };
+    forPeriod = (period) => {
+        let weatherForecastForPeriod = [];
+        this.weatherForecastsSet.forEach((element) => {
+                if (period.getDateFrom().getTime() <= element.getDate().getTime() &&
+                    element.getDate().getTime() <= period.getDateTo().getTime())
+                    weatherForecastForPeriod.push(element);
+            }
+        );
+        return new WeatherForecast(weatherForecastForPeriod);
+    };
+    including = (forecastData) => {
+        let forecastsDataForComparison = [];
+        this.weatherForecastsSet.forEach(element => {
+            forecastData.forEach((comparisonElement => {
+                if (Object.keys(element).length === Object.keys(comparisonElement).length
+                    && Object.keys(element).every(p => element[p] === comparisonElement[p])) {
+                    forecastsDataForComparison.push(element);
+                }
+            }));
+        });
+        return new WeatherForecast(forecastsDataForComparison);
+    };
+    convertToUsUnits = () => {
+        this.weatherForecastsSet.forEach((element => {
+            switch (element.getType()) {
+                case CELSIUS_TYPE:
+                    element.setUnit(FAHRENHEIT_UNIT);
+                    element.setType(FAHRENHEIT_TYPE);
+                    element.setValue((element.getValue() * 9 / 5) + 32);
+                    break;
+                case MM_TYPE:
+                    element.setUnit(IN);
+                    element.setType(IN_TYPE);
+                    element.setValue(element.getValue() * 25.4);
+                    break;
+                case MPS_TYPE:
+                    element.setUnit(MPH)
+                    element.setType(MPH_TYPE);
+                    element.setValue(element.getValue() * 2.237)
+                    break;
+                default:
+                    break;
+            }
+        }));
+    };
+    convertToInternationalUnits = () => {
+        this.weatherForecastsSet.forEach((element => {
+            switch (element.getType()) {
+                case FAHRENHEIT_TYPE:
+                    element.setUnit(CELSIUS_UNIT);
+                    element.setType(CELSIUS_TYPE);
+                    element.setValue((element.getValue() * 9 / 5) + 32);
+                    break;
+                case IN_TYPE:
+                    element.setUnit(MM);
+                    element.setType(MM_TYPE);
+                    element.setValue(element.getValue() * 25.4);
+                    break;
+                case MPH_TYPE:
+                    element.setUnit(MPS)
+                    element.setType(MPS_TYPE);
+                    element.setValue(element.getValue() * 2.237)
+                    break;
+                default:
+                    break;
+            }
+        }));
+    };
+    getAverageMinValue = () => {
+        let avgMinNumber = 0;
+        this.weatherForecastsSet.forEach(element => {
+            if (element.getMin() !== undefined) {
+                avgMinNumber += element.getMin();
+            }
+            return avgMinNumber / this.weatherForecastsSet.length;
+        });
+    }
+    getAverageMaxValue = () => {
+        let avgMaxNumber = 0;
+        this.weatherForecastsSet.weatherPredictionSet.forEach(element => {
+            if (element.getMax() !== undefined) {
+                avgMaxNumber += element.getMax();
+            }
+            return avgMaxNumber / this.weatherForecastsSet.length;
+        });
+    };
+    getPredictions = () => this.weatherForecastsSet.weatherPredictionSet;
+    setData = (forecastData) => this.weatherForecastsSet.concat(forecastData);
+}
+//TODO: remaining tests
