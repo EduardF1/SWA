bc28edfc436c5e2b7e3d2e4b8094fd76a8240091
EduardF1 <fischer_eduard@yahoo.com>
2021-09-19 00:58:56 +0200

    more tests

---
+++ assignments/assignment_1/exercise_1_1/src/PrecipitationPrediction.js
@@ -0,0 +1,43 @@
+const {Event} = require('../src/Event');
+const {DataType} = require('../src/DataType');
+const {WeatherPrediction} = require('../src/WeatherPrediction');
+const {MM_TYPE, MM, IN_TYPE, IN} = require("../../Constants");
+
+const PrecipitationPrediction = (unit, type, place, time, max, min, expectedTypes) => {
+    let state = {unit: unit, type: type, place: place,time:time, max: max, min: min, expectedTypes: expectedTypes};
+    let event = Event(state.place,state.time);
+    let dataType = DataType(state.unit, state.type);
+    let weatherPrediction = WeatherPrediction(dataType.getUnit(), dataType.getType(), event.getPlace(), event.getTime(), state.min, state.max);
+    const getExpectedTypes = () => new Array(state.expectedTypes);
+    const setExpectedTypes = (newExpectedTypes) => state.expectedTypes = newExpectedTypes;
+    const setWeatherPrediction = (newWeatherPrediction) => weatherPrediction = newWeatherPrediction;
+    const getWeatherPrediction = () => weatherPrediction;
+    const matches = (data) => (
+        data.getValue() === ((weatherPrediction.getMin() + weatherPrediction.getMax()) / 2) &&
+        data.getDataType().getType() === weatherPrediction.getDataType().getType() &&
+        data.getDataType().getUnit() === weatherPrediction.getDataType().getUnit() &&
+        data.getEvent().getTime() === weatherPrediction.getEvent().getTime() &&
+        data.getEvent().getPlace() === weatherPrediction.getEvent().getPlace()
+    );
+    const convertToInches = () => {
+        if (weatherPrediction.getDataType().getType() === MM_TYPE) {
+            weatherPrediction.getDataType().setUnit(IN);
+            weatherPrediction.setMin((weatherPrediction.getMin() / 25.4));
+            weatherPrediction.setMax((weatherPrediction.getMax() / 25.4));
+            weatherPrediction.getDataType().setType(IN_TYPE);
+        }
+    };
+    const convertToMM = () => {
+        if (weatherPrediction.getDataType().getType() === IN_TYPE) {
+            weatherPrediction.getDataType().setUnit(MM);
+            weatherPrediction.setMin((weatherPrediction.getMin() * 0.0393701));
+            weatherPrediction.setMax((weatherPrediction.getMax() * 0.0393701));
+            weatherPrediction.getDataType().setType(MM_TYPE);
+        }
+    };
+    return {...weatherPrediction, getExpectedTypes, setExpectedTypes, matches, convertToInches, convertToMM, getWeatherPrediction, setWeatherPrediction}
+}
+
+module.exports = {
+    PrecipitationPrediction
+}
