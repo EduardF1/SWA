4386d526c7b279092a5d16e5ab054dc5c0d42efb
EduardF1 <fischer_eduard@yahoo.com>
2021-10-05 16:08:36 +0200

    finish node extras

--- extras/node/index.js
+++ extras/node/index.js
@@ -6,36 +6,51 @@ const SERVER_PORT = parseInt(process.env.PORT) || 4200;
 
 
 server = http.createServer((request, response) => {
-    if (request.url === '/') {
-        fs.readFile(path.join(__dirname, 'public', 'index.html'), (error, content) => {
-            if (!error) {
-                response.writeHead(200, {'Content-Type': 'text/html'});
-                response.end(content);
-            } else {
-                console.error(error);
-            }
-        });
+    // Build file path
+    let filePath = path.join(__dirname, 'public', request.url === '/' ? 'index.html' : request.url);
+    // File Extension
+    let fileExtension = path.extname(filePath);
+    let contentType = 'text/html';
+    // Set content based on file type
+    switch (fileExtension) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        default:
+            break;
     }
 
-    if (request.url === '/about') {
-        fs.readFile(path.join(__dirname, 'public', 'about.html'), (error, content) => {
-            if (!error) {
-                response.writeHead(200, {'Content-Type': 'text/html'});
-                response.end(content);
+    // Read file
+    fs.readFile(filePath, ((error, content) => {
+        if (error) {
+            if (error.code === 'ENOENT') {
+                // Page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (error, content) => {
+                    response.writeHead(200, {'Content-Type': 'text/html'});
+                    response.end(content, 'utf8');
+                });
             } else {
-                console.error(error);
+                response.writeHead(500);
+                response.end(`Server Error: ${error.code}`);
             }
-        });
-    }
-
-    if (request.url === '/api/users') {
-       const users = [
-           {name: 'Bob Doe', age:30},
-           {name: 'Carl Luther', age: 40}
-       ];
-       response.writeHead(200, {'Content-Type': 'application/json'});
-       response.end(JSON.stringify(users));
-    }
+        }else {
+            // Success
+            response.writeHead(200, {'Content-Type': contentType});
+            response.end(content, 'utf8');
+        }
+    }));
 });
 
 server.listen(SERVER_PORT, () => console.log(`Server running on port: ${SERVER_PORT}`));
