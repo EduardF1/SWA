041349386c97b2a6a276b7ad94e3ad2dffc1ac21
EduardF1 <fischer_eduard@yahoo.com>
2021-10-13 22:13:44 +0200

    .

---
+++ extras/library/library.js
@@ -0,0 +1,22 @@
+function $(selector) {
+    const self = {
+        element: document.querySelector(selector),
+        html: () => self.element,
+        on: (event, callback) => {
+            document.addEventListener(event, callback)
+        },
+        hide: () => {
+            self.element.style.display = 'none';
+        },
+        attr: (name, value) => {
+            if(value == null){
+                self.element.getAttribute(name);
+            }else {
+                self.element.setAttribute(name, value);
+            }
+        }
+    }
+    return self;
+}
+
+$('h1').attr('class', 'new-class');
