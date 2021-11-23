8c56b6eda35bc3d8edf64d3a43c5b13d8bd0aae4
EduardF1 <fischer_eduard@yahoo.com>
2021-11-24 00:50:13 +0100

    .

--- Constants.js
+++ Constants.js
@@ -1,25 +1,52 @@
 // Global constants
-export const NEW_LINE = '\n';
-export const MM_UNIT = 'MM';
-export const MM_TYPE = 'Millimeters';
-export const IN_UNIT = 'IN';
-export const IN_TYPE = 'Inches';
-export const MPH_UNIT = 'MPH';
-export const MPH_TYPE = 'Miles per Hour';
-export const MPS_UNIT = 'MPS';
-export const MPS_TYPE = 'Meters per Second';
-export const FAHRENHEIT_TYPE = 'Fahrenheit';
-export const FAHRENHEIT_UNIT = '*F';
-export const CELSIUS_TYPE = 'Celsius';
-export const CELSIUS_UNIT = '*C';
-export const START_DATE = '1999-12-20';
-export const END_DATE = '2021-11-10';
-export const START_DATE_2 = '1950-01-20';
-export const END_DATE_2 = '2030-01-02';
-export const EMPTY_STRING = '';
-export const PLACES = ['Lvov', 'Munich', 'Moscow', 'Bucharest C', 'Sibiu', 'Burgas', 'Stalingrad', 'Tallinn', 'Chicago', 'Los Santos'];
-export const RAIN = 'Rain';
-export const FOG = 'Fog';
-export const SNOW = 'Snow';
-export const DIRECTIONS = ['North', 'South', 'West', 'East'];
-export const VALUES = [10, 11.7, 15, 20, 25, 28.3, 30, 35, 50, 100, 40];
+const NEW_LINE = '\n';
+const MM_UNIT = 'MM';
+const MM_TYPE = 'Millimeters';
+const IN_UNIT = 'IN';
+const IN_TYPE = 'Inches';
+const MPH_UNIT = 'MPH';
+const MPH_TYPE = 'Miles per Hour';
+const MPS_UNIT = 'MPS';
+const MPS_TYPE = 'Meters per Second';
+const FAHRENHEIT_TYPE = 'Fahrenheit';
+const FAHRENHEIT_UNIT = '*F';
+const CELSIUS_TYPE = 'Celsius';
+const CELSIUS_UNIT = '*C';
+const START_DATE = '1999-12-20';
+const END_DATE = '2021-11-10';
+const START_DATE_2 = '1950-01-20';
+const END_DATE_2 = '2030-01-02';
+const EMPTY_STRING = '';
+const PLACES = ['Lvov', 'Munich', 'Moscow', 'Bucharest C', 'Sibiu', 'Burgas', 'Stalingrad', 'Tallinn', 'Chicago', 'Los Santos'];
+const RAIN = 'Rain';
+const FOG = 'Fog';
+const SNOW = 'Snow';
+const DIRECTIONS = ['North', 'South', 'West', 'East'];
+const VALUES = [10, 11.7, 15, 20, 25, 28.3, 30, 35, 50, 100, 40];
+
+module.exports = {
+    RAIN,
+    FOG,
+    SNOW,
+    MM_UNIT,
+    MM_TYPE,
+    IN_UNIT,
+    IN_TYPE,
+    MPH_UNIT,
+    MPH_TYPE,
+    MPS_UNIT,
+    MPS_TYPE,
+    FAHRENHEIT_TYPE,
+    FAHRENHEIT_UNIT,
+    CELSIUS_TYPE,
+    CELSIUS_UNIT,
+    END_DATE,
+    START_DATE,
+    END_DATE_2,
+    START_DATE_2,
+    PLACES,
+    EMPTY_STRING,
+    DIRECTIONS,
+    VALUES,
+    NEW_LINE
+};
